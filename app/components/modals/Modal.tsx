44a291491b785159d3d18a16825881039503c880
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-07-20 00:13:43 +0300

    starting with modals

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: string;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  return <div>Modal</div>;
+};
+
+export default Modal;

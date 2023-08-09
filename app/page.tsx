63a3b7773a16ac139dc0170fe9715b670b43bcaa
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-08-09 23:34:10 +0300

    made the listing card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,2 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <div className='text-rose-500 text-2xl'>Hello Airbnb!</div>
-  )
-}
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";

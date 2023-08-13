42388b65cfde84f9fdc2fb0325b1b3eb5d15dea1
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-08-14 02:46:14 +0300

    fixed date-range issue

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -4,3 +4,33 @@ import getReservations from "@/app/actions/getReservations";
 
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
+
+import ListingClient from "./ListingClient";
+
+interface IParams {
+  listingId?: string;
+}
+
+const ListingPage = async ({ params }: { params: IParams }) => {
+  const listing = await getListingById(params);
+  const reservations = await getReservations(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <ListingClient
+        listing={listing}
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+};
